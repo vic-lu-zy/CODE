b3b87eeb4e2048f34b862ad7ad8502bac18af17f
Victor Lu <victor.lu@mail.mcgill.ca>
2015-06-24 15:50:25 -0700

    van01

--- data manipulation/logarithmicBinning.m
+++ data manipulation/logarithmicBinning.m
@@ -3,7 +3,9 @@ fspace = logspace(0,log10(500),20);
 fspace(1)=0;
 
 [~, binidx] = histc(freq,fspace);
+
 binidx(end)=binidx(end-1);
+
 means = accumarray(binidx, S, [], @mean);
 
 loglog(freq,S,fspace(1:end-1),means)
